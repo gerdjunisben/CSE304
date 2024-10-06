0819a0f19a8a6f8c33be5363df2fdf0159f7f671
Donato J Zampini <donato.zampini23@gmail.com>
2024-10-05 21:35:25 -0400

    Started creating lexer, wrote a function for creating tokens and reading program, having issues making my computer understand that it has ply installed so some difficulties running code. Tired and will test tomorrow.

--- HW3/decaf_lexer.py
+++ HW3/decaf_lexer.py
@@ -1,1 +1,39 @@
 import ply.lex as lex
+
+reserved_words = ["boolean", "break", "continue", "class", "do", "else"
+"extends", "false", "float", "for", "if", "int"
+"new", "null", "private", "public", "return", "static"
+"super", "this", "true", "void", "while"]
+
+tokens:list[str] = []
+
+# Tokenizes a line that contains the operator "::="
+def createToken(String:str):
+    newToken = String.split(":").pop(0)
+    if newToken not in tokens:
+        tokens.append(newToken)
+
+# Reads through entire program, passed as one long string
+def lexProgram(String:str):
+    lines:list[str] = str.split("\n")
+    for line in lines:
+        if "::=" in line:
+            createToken(line)
+
+lexer = lex.lex
+
+# Test it out
+data = '''
+3 + 4 * 10
+  + -20 *2
+'''
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok: 
+        break      # No more input
+    print(tok)
